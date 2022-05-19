874cc0ec0484c48b53be886c7f8079ecdd6d97db
Raquel <raquelribeirorios93@gmail.com>
2022-05-19 14:44:19 -0300

    Adicionando classes

---
+++ src/com/company/model/SaleItem.java
@@ -0,0 +1,46 @@
+package com.company.model;
+
+import java.math.BigDecimal;
+
+public class SaleItem {
+
+    private Long id;
+    private Long quantity;
+    private BigDecimal unitPrice;
+
+    public SaleItem(Long id, Long quantity, BigDecimal unitPrice) {
+        this.id = id;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public BigDecimal getPrice(){
+        return unitPrice.multiply(new BigDecimal(quantity));
+    }
+
+
+}
---
+++ src/com/company/model/Salesman.java
@@ -0,0 +1,42 @@
+package com.company.model;
+
+import java.math.BigDecimal;
+
+public class Salesman {
+
+        private String cpf;
+        private String name;
+        private BigDecimal salary;
+
+        public Salesman(String cpf, String name, BigDecimal salary) {
+            this.cpf = cpf;
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public String getCpf() {
+            return cpf;
+        }
+
+        public void setCpf(String cpf) {
+            this.cpf = cpf;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public BigDecimal getSalary() {
+            return salary;
+        }
+
+        public void setSalary(BigDecimal salary) {
+            this.salary = salary;
+        }
+
+
+}
